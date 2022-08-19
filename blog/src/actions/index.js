66c58f95bd2fd3c7e32f2c57df26134da766cffe
Kamil Jakóbczak <k_j1@vp.pl>
2022-08-19 11:48:47 +0200

    add fetching users

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,4 +1,20 @@
+import _ from 'lodash';
 import jsonPlaceholder from '../apis/jsonPlaceholder';
+
+export const fetchPostsAndUsers = () => {
+  return async (dispatch, getState) => {
+    await dispatch(fetchPosts());
+
+    // const userIds = _.uniq(_.map(getState().posts, 'userId'));
+    // userIds.forEach(id => dispatch(fetchUser(id)));
+
+    _.chain(getState().posts)
+      .map('userId')
+      .uniq()
+      .forEach(id => dispatch(fetchUser(id)))
+      .value();
+  };
+};
 
 export const fetchPosts = () => {
   return async dispatch => {
@@ -7,6 +23,26 @@ export const fetchPosts = () => {
     dispatch({ type: 'FETCH_POSTS', payload: response.data });
   };
 };
+
+// This code results in overfetching data:
+
+// export const fetchUser = id => {
+//   return async dispatch => {
+//     const response = await jsonPlaceholder.get(`/users/${id}`);
+
+//     dispatch({ type: 'FETCH_USER', payload: response.data });
+//   };
+// };
+
+//SOLUTION 1: problem - what happens when user data changes?
+
+// export const fetchUser = id => dispatch => _fetchUser(id, dispatch);
+
+// const _fetchUser = _.memoize(async (id, dispatch) => {
+//   const response = await jsonPlaceholder.get(`/users/${id}`);
+
+//   dispatch({ type: 'FETCH_USER', payload: response.data });
+// });
 
 export const fetchUser = id => {
   return async dispatch => {
