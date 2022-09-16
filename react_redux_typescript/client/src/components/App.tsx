495c1940a73732aea0d84d5da89f25efe3c16b6c
Kamil Jakóbczak <k_j1@vp.pl>
2022-09-16 12:21:07 +0200

    final commit for redux typescript project

---
+++ react_redux_typescript/client/src/components/App.tsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { Provider } from 'react-redux';
+import { store } from '../state';
+import RepositoriesList from './RepositoriesList';
+class App extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <div>
+          <h1>Search for a Package</h1>
+          <RepositoriesList />
+        </div>
+      </Provider>
+    );
+  }
+}
+
+export default App;
