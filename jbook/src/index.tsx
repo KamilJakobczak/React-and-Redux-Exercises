7415defeb66630ed7c8efcda69548f7f64d7cc21
Kamil Jakóbczak <k_j1@vp.pl>
2022-09-21 11:10:53 +0200

    add css files logic

--- jbook/src/index.tsx
+++ jbook/src/index.tsx
@@ -1,17 +1,18 @@
-import * as esbuild from "esbuild-wasm";
-import { useState, useEffect, useRef } from "react";
-import ReactDOM from "react-dom";
-import { unpkgPathPlugin } from "./plugins/unpkg-path-plugin";
+import * as esbuild from 'esbuild-wasm';
+import { useState, useEffect, useRef } from 'react';
+import ReactDOM from 'react-dom';
+import { fetchPlugin } from './plugins/fetch-plugin';
+import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
 
 const App = () => {
   const ref = useRef<any>();
-  const [input, setInput] = useState("");
-  const [code, setCode] = useState("");
+  const [input, setInput] = useState('');
+  const [code, setCode] = useState('');
 
   const startService = async () => {
     ref.current = await esbuild.startService({
       worker: true,
-      wasmURL: "/esbuild.wasm",
+      wasmURL: 'https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm',
     });
   };
   useEffect(() => {
@@ -24,13 +25,13 @@ const App = () => {
     }
 
     const result = await ref.current.build({
-      entryPoints: ["index.js"],
+      entryPoints: ['index.js'],
       bundle: true,
       write: false,
-      plugins: [unpkgPathPlugin(input)],
+      plugins: [unpkgPathPlugin(), fetchPlugin(input)],
       define: {
-        "process.env.NODE_ENV": '"production"',
-        global: "window",
+        'process.env.NODE_ENV': '"production"',
+        global: 'window',
       },
     });
     setCode(result.outputFiles[0].text);
@@ -50,4 +51,4 @@ const App = () => {
   );
 };
 
-ReactDOM.render(<App />, document.querySelector("#root"));
+ReactDOM.render(<App />, document.querySelector('#root'));
