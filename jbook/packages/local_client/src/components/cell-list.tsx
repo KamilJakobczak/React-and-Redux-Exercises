de6965834a379df9ac11b48b96f2afc4a4e487e8
Kamil Jakóbczak <k_j1@vp.pl>
2022-10-17 10:45:36 +0200

    add saving cells

--- jbook/packages/local_client/src/components/cell-list.tsx
+++ jbook/packages/local_client/src/components/cell-list.tsx
@@ -1,14 +1,26 @@
 import './cell-list.css';
-import { Fragment } from 'react';
+import { Fragment, useEffect } from 'react';
 import { useTypedSelector } from '../hooks/use-typed-selector';
 import AddCell from './add-cell';
 import CellListItem from './cell-list-item';
+import { useActions } from '../hooks/useActions';
+
 const CellList: React.FC = () => {
   const cells = useTypedSelector(({ cells: { order, data } }) => {
     return order.map(id => {
       return data[id];
     });
   });
+  const { fetchCells, saveCells } = useActions();
+
+  useEffect(() => {
+    fetchCells();
+  }, []);
+
+  useEffect(() => {
+    saveCells();
+  }, []);
+
   const renderedCells = cells.map(cell => (
     <Fragment key={cell.id}>
       <CellListItem key={cell.id} cell={cell} />
--- jbook/packages/local_client/src/state/action-creators/index.ts
+++ jbook/packages/local_client/src/state/action-creators/index.ts
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import { Dispatch } from 'redux';
 import Bundler from '../../bundler';
 import { ActionType } from '../action-types';
@@ -9,7 +10,8 @@ import {
   MoveCellAction,
   UpdateCellAction,
 } from '../actions';
-import { CellTypes } from '../cell';
+import { Cell, CellTypes } from '../cell';
+import { RootState } from '../reducers';
 
 export const deleteCell = (id: string): DeleteCellAction => {
   return {
@@ -69,5 +71,44 @@ export const createBundle = (cellId: string, input: string) => {
         },
       },
     });
+  };
+};
+
+export const fetchCells = () => {
+  return async (dispatch: Dispatch<Action>) => {
+    dispatch({ type: ActionType.FETCH_CELLS });
+
+    try {
+      const { data }: { data: Cell[] } = await axios.get('/cells');
+
+      dispatch({ type: ActionType.FETCH_CELLS_COMPLETE, payload: data });
+    } catch (err) {
+      if (err instanceof Error) {
+        dispatch({
+          type: ActionType.FETCH_CELLS_ERROR,
+          payload: err.message,
+        });
+      }
+    }
+  };
+};
+
+export const saveCells = () => {
+  return async (dispatch: Dispatch<Action>, getState: () => RootState) => {
+    const {
+      cells: { data, order },
+    } = getState();
+    const cells = order.map(id => data[id]);
+
+    try {
+      await axios.post('/cells', { cells });
+    } catch (err) {
+      if (err instanceof Error) {
+        dispatch({
+          type: ActionType.SAVE_CELLS_ERROR,
+          payload: err.message,
+        });
+      }
+    }
   };
 };
